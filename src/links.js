0b2aaa5667e6ff4353d2b85ad2592a684d58379e
chrisoney <christopher.oney@yahoo.com>
2020-09-13 12:41:14 -0700

    add line breaks

--- src/links.js
+++ src/links.js
@@ -7,7 +7,7 @@ const links = {
     'email': ''
   }, 
   'Marquis Bentley': {
-    'description':'I am a software engineer with expertise in JavaScript, React, Redux and Ruby on Rails. I love learning new technology and challenging myself to think both logically and creatively.  </br> When I\'m not programming I am likely playing basketball, reading a book or exploring my creativity through photography, music or writing!',
+    'description':'I am a software engineer with expertise in JavaScript, React, Redux and Ruby on Rails. I love learning new technology and challenging myself to think both logically and creatively.  <br /> When I\'m not programming I am likely playing basketball, reading a book or exploring my creativity through photography, music or writing!',
     'portfolio': 'https://www.marquisbentley.com/',
     'github': 'https://github.com/codedbyq',
     'linkedin': 'https://www.linkedin.com/in/marquisbentley/',
@@ -31,7 +31,7 @@ const links = {
     'email': 'mailto:johnlamll916@gmail.com'
   }, 
   'Chris Oney': {
-    'description':'Software development is something I\'m very passionate about. Combining my programming skills and creativity is extremely fulfilling. When I\'m not coding the day away I enjoy tabletop games, reading, and learning new skills of all types.',
+    'description':'Software development is something I\'m very passionate about. Combining my programming skills and creativity is extremely fulfilling. <br /> When I\'m not coding the day away I enjoy tabletop games, reading, and learning new skills of all types.',
     'portfolio': 'http://www.chrisoney.com',
     'github': 'https://github.com/chrisoney',
     'linkedin': 'https://www.linkedin.com/in/christopher-oney-317b84132/',
